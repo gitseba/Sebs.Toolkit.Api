0232bb4ee1cfe7d0cd1da12be67b46829102b045
ToSeba <seb.developing@gmail.com>
2023-05-28 21:37:08 +0300

    Add project files.

---
+++ Tests/Sebs.Toolkit.Api.UnitTests/Helpers/ApiHelperTests.cs
@@ -0,0 +1,23 @@
+﻿using Sebs.Toolkit.Api.Helpers;
+using Sebs.Toolkit.Api.UnitTests.Models;
+
+namespace Sebs.Toolkit.Api.UnitTests.Helpers
+{
+    /// <summary>
+    /// Purpose: 
+    /// Created by: sebde
+    /// Created at: 5/28/2023 9:20:17 PM
+    /// </summary>
+    public class ApiHelperTests
+    {
+        [Theory]
+        [InlineData("https://official-joke-api.appspot.com/random_joke")]
+        public async Task Should_Return_From_Api_An_Object_Model(string url)
+        {
+            var result = await ApiHelper
+                .GetDataFromAsync<OfficialJokeModel>(url);
+
+            Assert.NotNull(result.model);
+        }
+    }
+}
---
+++ Tests/Sebs.Toolkit.Api.UnitTests/Models/OfficialJokeModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Sebs.Toolkit.Api.UnitTests.Models
+{
+    /// <summary>
+    /// Purpose: 
+    /// Created by: sebde
+    /// Created at: 5/28/2023 9:24:01 PM
+    /// </summary>
+    public class OfficialJokeModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("type")]
+        public string? Type { get; set; }
+
+        [JsonProperty("setup")]
+        public string? Setup { get; set; }
+
+        [JsonProperty("punchline")]
+        public string? PunchLine { get; set; }
+    }
+}
